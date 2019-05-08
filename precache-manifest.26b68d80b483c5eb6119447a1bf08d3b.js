0dd96c56d420b20f2e9034c83117efc80b38801a
Stephen Wisner <swisner@purch.com>
2019-05-08 14:01:07 -0400

    Updates

---
+++ precache-manifest.26b68d80b483c5eb6119447a1bf08d3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741b938f43f4b3b9f0b488b35b112ea2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5af005e50a197993fbe",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "f5af005e50a197993fbe",
+    "url": "/static/js/main.50ac6f32.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "14c1621afe46159246181b6e801ef409",
+    "url": "/static/media/uncle-buck.14c1621a.gif"
+  }
+]);

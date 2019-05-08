cf2f2fbb2656c394da4dbf1d2cc8152f42b7fbac
Stephen Wisner <swisner@purch.com>
2019-05-08 13:59:36 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import uncleBuck from './uncle-buck.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Happy Birthday Sam Chan!</h1>
+        <img src={uncleBuck} alt="logo" />
+        <h2>Top 3 Reasons Sam Is Awesome:</h2>
+        <div style={{display: 'flex', textAlign: 'left'}}>
+          <div>
+            <p>1. She is willing to give a drum roll without needing to know why</p>
+            <p>2. She gets excited for other people who are putting garlic sauce on their Chicken and Rice</p>
+            <p>3. She writes awesome code, makes the team look good, and is super cool</p>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

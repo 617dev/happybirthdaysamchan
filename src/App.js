5782c35af619ca2a9c7a439b056b34a94d3dcb69
Alex <arosenthal@business.com>
2020-05-08 13:25:18 -0400

    add bday song and baby yoda

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import uncleBuck from './uncle-buck.gif';
-import './App.css';
-
+import ReactAudioPlayer from 'react-audio-player';
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
+
+import babyYoda from './baby-yoda-bday.gif';
+import happyBdayMp3 from './happyBday.mp3';
+import './App.css';
 
 function App() {
   const { width, height } = useWindowSize()
@@ -15,14 +17,16 @@ function App() {
     />
       <header className="App-header">
         <h1>Happy Birthday Sam Chan!</h1>
-        <img src={uncleBuck} alt="logo" />
+        <img src={babyYoda} alt="baby yoda" />
         <h2>Top 3 Reasons Sam Is Awesome:</h2>
         <div style={{display: 'flex', textAlign: 'left'}}>
           <div>
             <p>1. She is willing to give a drum roll without needing to know why</p>
             <p>2. She gets excited for other people who are putting garlic sauce on their Chicken and Rice</p>
             <p>3. She writes awesome code, makes the team look good, and is super cool</p>
+            <p>4. She is the @bdc_dev_garage social media manager, and brand police! 🙌</p>
           </div>
+          <ReactAudioPlayer src={happyBdayMp3} autoPlay={true}/>
         </div>
       </header>
     </div>
